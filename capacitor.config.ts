9957fc1114e7bd38ba2ab2bc202ff781ed88dd65
AngeloGrb <220416@studierende.htl-donaustadt.at>
2026-03-23 11:49:40 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'who-is-who-htl',
+  webDir: 'www'
+};
+
+export default config;
